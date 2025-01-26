38f087df96f5b293bc99f3dd31ffc18993884445
saniya potdar <saniyapotdar03@gmail.com>
2025-01-26 11:57:51 +0530

    Add files via upload

---
+++ arith.py
@@ -0,0 +1,14 @@
+#This is a code of arithmatic operations using python
+
+a=int(input("Enter 1st number= "))
+b=int(input("Enter 2nd number= "))
+
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+
+print("Addition is=",add)
+print("Substraction is=",sub)
+print("Multiplication is=",mul)
+print("Division is=",div)
